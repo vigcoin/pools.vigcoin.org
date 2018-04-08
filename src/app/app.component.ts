54cfe3d2242acf16af28746de9df8b4926e8d072
calidion <calidion@gmail.com>
2018-04-09 02:07:06 +0800

    enabled nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
 import { DataService } from './data.service';
-
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 
 @Component({
@@ -13,7 +13,7 @@ import { Observable } from 'rxjs/Rx';
 export class AppComponent implements OnInit {
   title = '田一块 VIG 矿池';
   status
-  public constructor(private titleService: Title, private meta: Meta, private dataService: DataService) {
+  public constructor(private titleService: Title, private meta: Meta, private dataService: DataService, private router: Router) {
     this.titleService.setTitle(this.title);
     this.meta.addTag({ name: 'keywords', content: 'VIG,矿池,BITCOIN,比特币' });
   }
@@ -24,20 +24,7 @@ export class AppComponent implements OnInit {
     this.dataService.get(this.dataService.config.api + '/stats').subscribe(data => {
       if (Object.keys(data).length > 0) {
         this.status = data;
-        // this.updateStatus();
       }
     });
   }
-
-  // updateStatus() {
-  //   this.status.network.hashrate = this.status.network.difficulty / this.status.config.coinDifficultyTarget;
-  //   this.status.network.hashrateReadable = this.dataService.hashRateWithUnit(this.status.network.difficulty / this.status.config.coinDifficultyTarget);
-  //   this.status.network.lastBlockTime = this.dataService.timeAgo(this.status.network.timestamp);
-  //   this.status.network.lastReward = this.dataService.getReadableCoins(this.status, this.status.network.reward, 4);
-  //   this.status.network.lastHashExplorerUrl = this.dataService.hashToUrl(this.status, this.status.network.hash);
-  //   this.status.pool.hashrateReadable = this.dataService.hashRateWithUnit(this.status.pool.hashrate);
-  //   this.status.pool.lastBlockTime = this.dataService.timeAgo(this.status.pool.lastBlockFound / 1000);
-  //   this.status.pool.blockTime = this.dataService.getReadableTime(this.status.network.difficulty / this.status.pool.hashrate);
-  // }
-
 }
