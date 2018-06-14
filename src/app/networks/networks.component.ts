62426d4132785c424a34aaa16a6bb0ad4ef9d508
549869500 <33763887+549869500@users.noreply.github.com>
2018-06-14 18:44:16 +0800

    一些语法规范

--- src/app/networks/networks.component.ts
+++ src/app/networks/networks.component.ts
@@ -42,12 +42,12 @@ export class NetworksComponent implements OnInit {
     const NETWORK_STAT_MAP = new Map(this.dataService.config.networkStat[this.status.config.symbol.toLowerCase()]);
     NETWORK_STAT_MAP.forEach((url, host, map) => {
       $.getJSON(url + '/stats', (data, textStatus, jqXHR) => {
-	      var isCurHost = host.toString().indexOf(curHost)>=0;
+	    var isCurHost = host.toString().indexOf(curHost)>=0;
         $('#network_rows').append('<tr>' +
-          '<td id=host-' + host + '><a target=blank href=http://' + host + '>' + (isCurHost?('<code>'+host+'</code>') : host) + '</a> (' + data.config.symbol + ')</td>' +
+          '<td id=host-' + host + '><a target=blank href=http://' + host + '>' + (isCurHost ? ('<code>' + host + '</code>') : host) + '</a> (' + data.config.symbol + ')</td>' +
           '<td id=height-' + host + '>' + data.network.height + '</td>' +
           '<td id=hashrate-' + host + '>' + data.pool.hashrate + '&nbsp;H/s</td>' +
-          '<td id=miners-' + host + '>' + data.pool.miners +'</td>' +
+          '<td id=miners-' + host + '>' + data.pool.miners + '</td>' +
           '<td id=totalFee-' + host + '>' + this.calculateTotalFee(data) + '%</td>' +
           '<td id=lastBlockFound-' + host + '>' + new Date(parseInt(data.pool.lastBlockFound)).toLocaleString() + '</td>' +
           '</tr>');
